8e35f1a7db5b6b32d31ae15d0b87319d5a72ef11
pamuditha <pamudithasenanayake@gmail.com>
2025-07-31 06:16:45 +0530

    daatabse ssl changed to rejectunauthorized: false state

--- db.js
+++ db.js
@@ -9,7 +9,9 @@ const pool = new pg.Pool({
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-    ssl: false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
